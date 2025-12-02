24f3f9341c8b095fc25a12ed353721c7ea60dd7e
CamilaCxstillo <camila.castillo@newtel.com.sv>
2025-12-02 16:58:02 -0600

    Editando home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home.component',
   imports: [],
+  templateUrl: './home.component.html',
 
 })
 export class HomeComponent { 
