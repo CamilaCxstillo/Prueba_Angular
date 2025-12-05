d9dc25a14917baa9e707a9346dcae010aa0d4525
CamilaCxstillo <camila.castillo@newtel.com.sv>
2025-12-05 17:20:30 -0600

    Create api.ts

---
+++ src/app/interfaces/api.ts
@@ -0,0 +1,14 @@
+export interface OmdbMovie {
+  imdbID: string;
+  Title: string;
+  Year: string;
+  Poster: string;
+  Type: string;
+}
+
+export interface OmdbSearchResponse {
+  Search?: OmdbMovie[];
+  totalResults?: string;
+  Response: "True" | "False";
+  Error?: string;
+}
