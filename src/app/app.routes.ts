46944690aa706c2169bccbc6e5dd7bf734303bec
CamilaCxstillo <camila.castillo@newtel.com.sv>
2025-12-05 11:41:13 -0600

    Update app.routes.ts
    Agregando rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,25 @@ import { HomeComponent } from './pages/home/home.component/home.component';
 
 export const routes: Routes = [
   { path: '',
-    component: HomeComponent },
+    component: HomeComponent 
+  },
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./pages/search/search.component').then(m => m.SearchComponent)
+  },
+  {
+    path: 'history',
+    loadComponent: () =>
+      import('./pages/history/history.component').then(m => m.HistoryComponent)
+  },
+  {
+    path: 'movie/:id',
+    loadComponent: () =>
+      import('./pages/movie-detail/movie-detail.component').then(m => m.DetailsComponent)
+  },
+
+  { path: '**', 
+    redirectTo: '' 
+  },
 ];
