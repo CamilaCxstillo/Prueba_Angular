905f3a790c60420f30d6c2769624818e5033057f
CamilaCxstillo <camila.castillo@newtel.com.sv>
2025-12-06 09:52:14 -0600

    Update history.component.ts

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,1 +1,20 @@
 
+import { MovieService } from './../../service/movie.service';
+import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+
+
+@Component({
+  selector: 'app-history',
+  standalone: true,
+  imports: [NgFor, NgIf],
+  templateUrl: './history.component.html',
+})
+export class HistoryComponent {
+  constructor(public svc: MovieService) { }
+
+
+  clear() {
+    this.svc.clearHistory();
+  }
+}
