cb5f11be190166f16f38f8f9833c3f3125e6679f
CamilaCxstillo <camila.castillo@newtel.com.sv>
2025-12-06 12:18:31 -0600

    Create loading-spinner.component.ts

---
+++ src/app/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mh-loading-spinner',
+  standalone: true,
+  template: `<div class="text-center p-4"><span class="loading loading-spinner loading-lg"></span></div>`
+})
+export class LoadingSpinnerComponent {}
